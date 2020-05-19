c87342e6bed6729b4fd2f78df19eade098dd100d
SunilPrakash <sunil.prakash@talentica.com>
2020-05-19 17:00:47 +0530

    Updates

---
+++ precache-manifest.4a720aadb650892ffe8eb109a74bc0ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7d7c7f4597dfb4d6e4301e48409efb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b10a0c4095dd2c0bfab",
+    "url": "/static/css/2.2e8c90f4.chunk.css"
+  },
+  {
+    "revision": "2e70abca7d6c93ae3842",
+    "url": "/static/css/main.a6d1b045.chunk.css"
+  },
+  {
+    "revision": "8b10a0c4095dd2c0bfab",
+    "url": "/static/js/2.42024936.chunk.js"
+  },
+  {
+    "revision": "2e70abca7d6c93ae3842",
+    "url": "/static/js/main.554d0cb3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
